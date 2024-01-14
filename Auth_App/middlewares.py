6186af4d78b1626848a1cfa838f3b2739d3c683e
aditya027 <adityaraj2798@gmail.com>
2024-01-15 00:17:47 +0530

    Initial commit

---
+++ Auth_App/middlewares.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect
+# *********** Authenticated user ***********
+# creating a decorator
+
+def auth(view_function):
+    def wrapped_view(request,*args,**kwargs):
+        if request.user.is_authenticated==False:
+            return redirect('login')
+        return view_function(request,*args,**kwargs)
+    return wrapped_view
+
+# *********** guest user ***********
+# creating a decorator
+
+def guest(view_function):
+    def wrapped_view(request,*args,**kwargs):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return view_function(request,*args,**kwargs)
+    return wrapped_view
